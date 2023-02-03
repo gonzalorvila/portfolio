e236aee97a8380ebeb600dcea373ff7df5370b34
Gonzalo Rodriguez-Vila <gonzalo.rodriguez-vila@bundlar.com>
2023-02-03 16:10:22 -0600

    Building Education Page - Created UI for education page - Added responsiveness with HostListener - Fixed route in header to route to Education

--- src/app/core/core.component.ts
+++ src/app/core/core.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+
+export interface Tile {
+  cols: number;
+  rows: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-core',
   templateUrl: './core.component.html',
