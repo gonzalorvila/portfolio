6957f2a759f8c99af3fe80843e7ac15bc0759c70
Gonzalo Rodriguez-Vila <gonzalo.rodriguez-vila@bundlar.com>
2023-03-15 16:49:00 -0500

    Functionality of Contact page - Connected email form to formspree email api - Contact form should now send me emails - Updated styles of contact page

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-contact',
@@ -7,4 +10,26 @@ import { Component } from '@angular/core';
 })
 export class ContactComponent {
 
+  constructor(
+    private http: HttpClient,
+    private snackBar: MatSnackBar) {}
+
+  public onSubmit(contactForm: NgForm) {
+    if (contactForm.valid) {
+      const email = contactForm.value;
+      const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+      this.http.post('https://formspree.io/f/mbjekwey',
+        { name: email.name, replyto: email.email, message: email.message },
+        { 'headers': headers}).subscribe(response => {
+            if (response) {
+              this.snackBar.open('Your email was sent!', 'Dismiss', {
+                duration: 5000,
+                horizontalPosition: 'end',
+                panelClass: ['email-sent-snackbar']
+              })
+            }
+          }
+        );
+    }
+  }
 }
