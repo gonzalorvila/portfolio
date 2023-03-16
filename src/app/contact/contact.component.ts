51a353a897ea3421b12dcbb5b59f0f64fc863781
Gonzalo Rodriguez-Vila <gonzalo.rodriguez-vila@bundlar.com>
2023-03-16 12:35:56 -0500

    Cleaning up Contact page - Contact form now resets after email is sent - Extracted logic for after email is sent into a separate method - Added comments to contact template and component - Organized contact stylesheet

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -15,21 +15,31 @@ export class ContactComponent {
     private snackBar: MatSnackBar) {}
 
   public onSubmit(contactForm: NgForm) {
+    // If the form is valid, submit message to Formspree API
     if (contactForm.valid) {
       const email = contactForm.value;
       const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
       this.http.post('https://formspree.io/f/mbjekwey',
         { name: email.name, replyto: email.email, message: email.message },
         { 'headers': headers}).subscribe(response => {
+            // If message was sent successfully, notify the user
             if (response) {
-              this.snackBar.open('Your email was sent!', 'Dismiss', {
-                duration: 5000,
-                horizontalPosition: 'end',
-                panelClass: ['email-sent-snackbar']
-              })
+              this.afterEmailSent();
             }
           }
         );
     }
+  }
+
+  private afterEmailSent() {
+    // Open snackbar to alert the user
+    this.snackBar.open('Your email was sent!', 'Dismiss', {
+      duration: 5000,
+      horizontalPosition: 'end',
+      panelClass: ['email-sent-snackbar']
+    });
+
+    // Clear the form
+    (<HTMLFormElement>document.getElementById('contact-form')).reset();
   }
 }
